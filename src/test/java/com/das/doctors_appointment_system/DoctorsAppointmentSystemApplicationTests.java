c2b3137146c7f8867efdd7a95fea35e619c6619d
Smith Jiue <smithjiue69@gmail.com>
2025-08-13 20:11:40 +0530

    Base project for dev environment

---
+++ src/test/java/com/das/doctors_appointment_system/DoctorsAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.das.doctors_appointment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
